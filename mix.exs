06b88d94990388c925c6b9ae1e77027b77fdf559
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-07-21 09:58:15 +0200

    Install dialyxir

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Core.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
