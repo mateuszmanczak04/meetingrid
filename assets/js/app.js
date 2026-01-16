fb0a1f748fb85e3c2682cbb46aa27ba78a7ca680
Mateusz Mańczak <mateuszmanczak@icloud.com>
2026-01-16 14:02:39 +0100

    Remove unneeded line

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,11 +21,8 @@ import 'phoenix_html';
 import { Socket } from 'phoenix';
 import { LiveSocket } from 'phoenix_live_view';
 import topbar from '../vendor/topbar';
-import CopyToClipboard from './copy_to_clipboard';
 
-const Hooks = {
-	CopyToClipboard,
-};
+const Hooks = {};
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content');
 let liveSocket = new LiveSocket('/live', Socket, {
