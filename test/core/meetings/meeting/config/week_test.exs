a329b5014d59a8863441bcf6b7f866f839de6368
Mateusz Mańczak <mateuszmanczak@icloud.com>
2026-02-11 19:48:22 +0100

    Use false by default for Week.include_weekends

--- test/core/meetings/meeting/config/week_test.exs
+++ test/core/meetings/meeting/config/week_test.exs
@@ -4,27 +4,12 @@ defmodule Core.Meetings.Meeting.Config.WeekTest do
   alias Core.Meetings.Meeting.Config.Week
 
   describe "changeset/2" do
-    test "validates required fields" do
-      changeset = Week.changeset(%Week{}, %{})
-
-      refute changeset.valid?
-      assert %{include_weekends: ["can't be blank"]} = errors_on(changeset)
-    end
-
     test "accepts valid attributes" do
       attrs = %{"include_weekends" => true}
 
       changeset = Week.changeset(%Week{}, attrs)
 
       assert changeset.valid?
-    end
-
-    test "casts include_weekends field correctly" do
-      attrs = %{"include_weekends" => false}
-
-      changeset = Week.changeset(%Week{}, attrs)
-
-      assert changeset.changes.include_weekends == false
     end
 
     test "rejects invalid mode value" do
@@ -36,9 +21,10 @@ defmodule Core.Meetings.Meeting.Config.WeekTest do
       assert %{mode: ["is invalid"]} = errors_on(changeset)
     end
 
-    test "sets default mode to :week" do
+    test "sets default values" do
       config = %Week{}
       assert config.mode == :week
+      assert config.include_weekends == false
     end
   end
 end
