48358b62ba61fd73c11d63b921f1f30b271f2375
Mateusz Mańczak <mateuszmanczak@icloud.com>
2026-02-24 18:23:23 +0100

    Use <.form> with changeset in /meetings/new

--- lib/core_web/live/meetings/new_live.ex
+++ lib/core_web/live/meetings/new_live.ex
@@ -4,12 +4,40 @@ defmodule CoreWeb.Meetings.NewLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    # TODO: use full changeset for all fields, not only title
+    data = %{}
+    types = %{title: :binary}
+    params = %{title: ""}
+
+    form =
+      {data, types}
+      |> Ecto.Changeset.cast(params, Map.keys(types))
+      |> Ecto.Changeset.validate_required(Map.keys(types))
+      |> to_form(as: :meeting)
+
+    {:ok, assign(socket, :form, form)}
   end
 
   @impl true
-  def handle_event("create", params, socket) do
-    case Meetings.create_meeting(socket.assigns.current_user, params) do
+  def handle_event("validate", %{"meeting" => attrs}, socket) do
+    data = %{}
+    types = %{title: :binary}
+
+    form =
+      {data, types}
+      |> Ecto.Changeset.cast(attrs, Map.keys(types))
+      |> Ecto.Changeset.validate_required(Map.keys(types))
+      |> to_form(as: :meeting, action: :validate)
+
+    {:noreply, assign(socket, :form, form)}
+  end
+
+  @impl true
+  def handle_event("submit", %{"meeting" => %{"title" => title}} = attrs, socket) do
+    # TODO
+    attrs = attrs |> Map.drop(["meeting"]) |> Map.put("title", title)
+
+    case Meetings.create_meeting(socket.assigns.current_user, attrs) do
       {:ok, attendee} ->
         {:noreply, push_navigate(socket, to: ~p"/meetings/#{attendee.meeting.id}")}
 
